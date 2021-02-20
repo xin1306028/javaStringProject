50055984caecc4f15fb66dc8ff405f6cd3d5d817
xin1306028 <xin1306028@gmail.com>
2021-02-20 12:21:04 -0800

    adding consonant class to consonant branch

--- src/javaStringProject/Consonants.java
+++ src/javaStringProject/Consonants.java
@@ -1,5 +1,19 @@
 package javaStringProject;
+import java.util.Scanner;
 
 public class Consonants {
-
+  public static void main(String args[]) {
+	  Scanner input = new Scanner(System.in);
+	  String str = input.nextLine();
+	  char[] array = str.toLowerCase().toCharArray();
+	  int count = 0;
+	  for(int i = 0; i < str.length(); i++) {
+		  if(array[i] != 'a' && array[i] != 'e' && array[i] != 'i' && array[i] != 'o' && array[i] != 'u') {
+			  count++;
+			  array[i] = '*';
+		  }
+	  }
+	  System.out.println("there are " + count + " consonant in the sentence" );
+	  System.out.println("the new string is " +String.valueOf(array));
+  }
 }
