02c833d14f0f5e2999c3728bffb98d3deb5a848b
xin1306028 <xin1306028@gmail.com>
2021-02-20 12:10:17 -0800

    adding words class to words branch

--- src/javaStringProject/Words.java
+++ src/javaStringProject/Words.java
@@ -1,5 +1,11 @@
 package javaStringProject;
+import java.util.Scanner;
 
 public class Words {
-
+   public static void main(String args[]) {
+	Scanner input = new Scanner(System.in);
+	String str = input.nextLine();
+	System.out.println("length of words is " +str.length());
+	
+}
 }
