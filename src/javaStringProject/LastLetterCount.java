93b76bfac4532270d7aa1bddaa99b01788b574ae
xin1306028 <xin1306028@gmail.com>
2021-02-20 12:52:28 -0800

    adding lastlettercount class to lastlettercount branch

--- src/javaStringProject/LastLetterCount.java
+++ src/javaStringProject/LastLetterCount.java
@@ -1,5 +1,28 @@
 package javaStringProject;
+import java.util.Scanner;
 
 public class LastLetterCount {
-
+   public static void main(String args[]) {
+	   
+	   Scanner input = new Scanner(System.in);
+	   String str = input.nextLine();
+	   String lower = str.toLowerCase();
+	   String firsts = null;
+	   String firsty = null;
+	  
+	   for(int i = 0; i < lower.length(); i++) {
+		   if(lower.charAt(i) == 's') {
+			   if(firsts == null) {
+				   firsts = lower.substring(0,i+1);
+			   }
+			   
+		   }else if(lower.charAt(i) == 'y') {
+			   if(firsty == null) {
+			   firsty = lower.substring(0, i+1);
+		   }
+		   }
+		  
+	   }
+	   System.out.println("there are two words swapped: " +firsts + " and " + firsty);
+   }
 }
