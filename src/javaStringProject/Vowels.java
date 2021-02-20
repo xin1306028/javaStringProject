c64d5d4027e312f2e837d338330ed476b0df442d
xin1306028 <xin1306028@gmail.com>
2021-02-20 11:51:56 -0800

    adding Vowels class to Vowels branch

--- src/javaStringProject/Vowels.java
+++ src/javaStringProject/Vowels.java
@@ -1,5 +1,27 @@
 package javaStringProject;
+import java.util.Scanner;
 
 public class Vowels {
-
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		String sentence = input.nextLine();
+		int count = countVowels(sentence);
+		System.out.println("number of Vowels is " +count);
+	}
+	
+	public static int countVowels(String sentence) {
+		String lower = sentence.toLowerCase();
+		int count = 0;
+		for(int i = 0; i < sentence.length(); i++) {
+			char letter = lower.charAt(i);
+			if(letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u') {
+				count++;
+			}
+		}
+		return count;
+		
+	}
+  
 }
+
+
